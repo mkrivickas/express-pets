ba3bb1d8d179e4df1749c6dec3a9c0760394d587
Mindaugas Krivickas <mkrivickas@gmail.com>
2024-04-22 18:35:24 +0300

    End of day progress for contact form feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,35 @@
 async function start() {
-  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-  const weatherData = await weatherPromise.json()
-
-  const ourTemperature = weatherData.properties.periods[0].temperature
-  document.querySelector("#temperature-output").textContent = ourTemperature
-
+  const weatherPromise = await fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=apparent_temperature"
+  );
+  const weatherData = await weatherPromise.json();
+  const ourTemperature = weatherData.hourly.apparent_temperature[12];
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
-start()
+start();
 
 // pet filter button code
-const allButtons = document.querySelectorAll(".pet-filter button")
+const allButtons = document.querySelectorAll(".pet-filter button");
 
-allButtons.forEach(el => {
-  el.addEventListener("click", handleButtonClick)
-})
+allButtons.forEach((el) => {
+  el.addEventListener("click", handleButtonClick);
+});
 
 function handleButtonClick(e) {
   // remove active class from any and all buttons
-  allButtons.forEach(el => el.classList.remove("active"))
+  allButtons.forEach((el) => el.classList.remove("active"));
 
   // add active class to the specific button that just got clicked
-  e.target.classList.add("active")
+  e.target.classList.add("active");
 
   // actually filter the pets down below
-  const currentFilter = e.target.dataset.filter
-  document.querySelectorAll(".pet-card").forEach(el => {
+  const currentFilter = e.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
     if (currentFilter == el.dataset.species || currentFilter == "all") {
-      el.style.display = "grid"
+      el.style.display = "grid";
     } else {
-      el.style.display = "none"
+      el.style.display = "none";
     }
-  })
+  });
 }
